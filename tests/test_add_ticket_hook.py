abad5d14f82dd2b6777d7f855d112ab3ca49a29e
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2019-07-11 22:50:53 +0300

    test for tag checking

--- tests/test_add_ticket_hook.py
+++ tests/test_add_ticket_hook.py
@@ -1,4 +1,10 @@
+import pytest
 from add_ticket_hook import __version__, add_ticket
+
+
+@pytest.fixture
+def options():
+    return add_ticket.Options(possible_tags=("test",), strict=False)
 
 
 def test_version():
@@ -7,3 +13,7 @@ def test_version():
 
 def test_parse_tag():
     assert add_ticket.parse_tag("test-1235: anothu") == "test"
+
+
+def test_is_tagged(options):
+    assert add_ticket.is_tagged("test-1235: anothu", options) is True
