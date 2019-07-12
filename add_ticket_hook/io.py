131a689705e63100df800992a83dcead2d7fa659
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2019-07-12 15:50:50 +0300

    added missing io.py file

---
+++ add_ticket_hook/io.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def read_commit_message(filename):
+    # type: (str) -> str
+    """ Read original commit message from file
+    """
+    with open(filename, "r+") as f:
+        return f.read().strip()
+
+
+def write_commit_message(filename, message):
+    # type: (str, str) -> None
+    """ Write new commit message to file
+    """
+    with open(filename, "r+") as f:
+        f.seek(0)
+        f.write(message)
+        f.truncate()
+
+
+def get_current_branch_name():
+    # type: () -> str
+    try:
+        branch_name = str(
+            subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        )
+        return branch_name.strip()
+    except (RuntimeError, AttributeError):
+        return ""
